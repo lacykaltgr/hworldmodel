9a4ef0fe63bd573b4505af22fee40f0f0306ef55
Laci <freundl0509@gmail.com>
2024-07-03 18:03:27 +0000

    isaac wrapper progress

--- isaac/wrapper.py
+++ isaac/wrapper.py
@@ -42,7 +42,7 @@ Vectorized environment wrapper.
 """
 
 
-class GymIsaacWrapper(VecEnv):
+class GymIsaacWrapper(VectorEnv):
     """Wraps around Isaac Lab environment for gymnasium.VectorEnv.
 
     The following changes are made to the environment to make it compatible with Isaac Lab:
@@ -77,30 +77,28 @@ class GymIsaacWrapper(VecEnv):
         Raises:
             ValueError: When the environment is not an instance of :class:`ManagerBasedRLEnv`.
         """
-        # check that input is valid
+        # Check that input is valid
         if not isinstance(env.unwrapped, ManagerBasedRLEnv) and not isinstance(env.unwrapped, DirectRLEnv):
             raise ValueError(
                 "The environment must be inherited from ManagerBasedRLEnv or DirectRLEnv. Environment type:"
                 f" {type(env)}"
             )
-        # initialize the wrapper
+        # Initialize the wrapper
         self.env = env
-        # collect common information
+        # Collect common information
         self.num_envs = self.unwrapped.num_envs
         self.sim_device = self.unwrapped.device
         self.render_mode = self.unwrapped.render_mode
 
-        # obtain gym spaces
-        # note: stable-baselines3 does not like when we have unbounded action space so
-        #   we set it to some high value here. Maybe this is not general but something to think about.
+        # Obtain gym spaces
         observation_space = self.unwrapped.single_observation_space["policy"]
         action_space = self.unwrapped.single_action_space
-        if isinstance(action_space, gym.spaces.Box) and not action_space.is_bounded("both"):
-            action_space = gym.spaces.Box(low=-100, high=100, shape=action_space.shape)
+        if isinstance(action_space, spaces.Box) and not action_space.is_bounded("both"):
+            action_space = spaces.Box(low=-100, high=100, shape=action_space.shape)
 
-        # initialize vec-env
-        VecEnv.__init__(self, self.num_envs, observation_space, action_space)
-        # add buffer for logging episodic information
+        # Initialize vec-env
+        VectorEnv.__init__(self, self.num_envs, observation_space, action_space)
+        # Add buffer for logging episodic information
         self._ep_rew_buf = torch.zeros(self.num_envs, device=self.sim_device)
         self._ep_len_buf = torch.zeros(self.num_envs, device=self.sim_device)
 
@@ -111,10 +109,6 @@ class GymIsaacWrapper(VecEnv):
     def __repr__(self):
         """Returns the string representation of the wrapper."""
         return str(self)
-
-    """
-    Properties -- Gym.Wrapper
-    """
 
     @classmethod
     def class_name(cls) -> str:
@@ -129,10 +123,6 @@ class GymIsaacWrapper(VecEnv):
         """
         return self.env.unwrapped
 
-    """
-    Properties
-    """
-
     def get_episode_rewards(self) -> list[float]:
         """Returns the rewards of all the episodes."""
         return self._ep_rew_buf.cpu().tolist()
@@ -141,49 +131,44 @@ class GymIsaacWrapper(VecEnv):
         """Returns the number of time-steps of all the episodes."""
         return self._ep_len_buf.cpu().tolist()
 
-    """
-    Operations - MDP
-    """
-
     def seed(self, seed: int | None = None) -> list[int | None]:  # noqa: D102
         return [self.unwrapped.seed(seed)] * self.unwrapped.num_envs
 
-    def reset(self) -> VecEnvObs:  # noqa: D102
-        obs_dict, _ = self.env.reset()
-        # convert data types to numpy depending on backend
+    def reset(self, seed: int | None = None, options: dict = None) -> VecEnvObs:  # noqa: D102
+        obs_dict, _ = self.env.reset(seed=seed, options=options)
+        # Convert data types to numpy depending on backend
         return self._process_obs(obs_dict)
 
     def step_async(self, actions):  # noqa: D102
-        # convert input to numpy array
+        # Convert input to numpy array
         if not isinstance(actions, torch.Tensor):
             actions = np.asarray(actions)
             actions = torch.from_numpy(actions).to(device=self.sim_device, dtype=torch.float32)
         else:
             actions = actions.to(device=self.sim_device, dtype=torch.float32)
-        # convert to tensor
+        # Store the actions
         self._async_actions = actions
 
     def step_wait(self) -> VecEnvStepReturn:  # noqa: D102
-        # record step information
+        # Record step information
         obs_dict, rew, terminated, truncated, extras = self.env.step(self._async_actions)
-        # update episode un-discounted return and length
+        # Update episode un-discounted return and length
         self._ep_rew_buf += rew
         self._ep_len_buf += 1
-        # compute reset ids
+        # Compute reset ids
         dones = terminated | truncated
         reset_ids = (dones > 0).nonzero(as_tuple=False)
 
-        # convert data types to numpy depending on backend
-        # note: ManagerBasedRLEnv uses torch backend (by default).
+        # Convert data types to numpy depending on backend
         obs = self._process_obs(obs_dict)
         rew = rew.detach().cpu().numpy()
         terminated = terminated.detach().cpu().numpy()
         truncated = truncated.detach().cpu().numpy()
         dones = dones.detach().cpu().numpy()
-        # convert extra information to list of dicts
+        # Convert extra information to list of dicts
         infos = self._process_extras(obs, terminated, truncated, extras, reset_ids)
 
-        # reset info for terminated environments
+        # Reset info for terminated environments
         self._ep_rew_buf[reset_ids] = 0
         self._ep_len_buf[reset_ids] = 0
 
@@ -193,15 +178,15 @@ class GymIsaacWrapper(VecEnv):
         self.env.close()
 
     def get_attr(self, attr_name, indices=None):  # noqa: D102
-        # resolve indices
+        # Resolve indices
         if indices is None:
             indices = slice(None)
             num_indices = self.num_envs
         else:
             num_indices = len(indices)
-        # obtain attribute value
+        # Obtain attribute value
         attr_val = getattr(self.env, attr_name)
-        # return the value
+        # Return the value
         if not isinstance(attr_val, torch.Tensor):
             return [attr_val] * num_indices
         else:
@@ -212,11 +197,8 @@ class GymIsaacWrapper(VecEnv):
 
     def env_method(self, method_name: str, *method_args, indices=None, **method_kwargs):  # noqa: D102
         if method_name == "render":
-            # gymnasium does not support changing render mode at runtime
             return self.env.render()
         else:
-            # this isn't properly implemented but it is not necessary.
-            # mostly done for completeness.
             env_method = getattr(self.env, method_name)
             return env_method(*method_args, indices=indices, **method_kwargs)
 
@@ -226,15 +208,11 @@ class GymIsaacWrapper(VecEnv):
     def get_images(self):  # noqa: D102
         raise NotImplementedError("Getting images is not supported.")
 
-    """
-    Helper functions.
-    """
-
     def _process_obs(self, obs_dict: torch.Tensor | dict[str, torch.Tensor]) -> np.ndarray | dict[str, np.ndarray]:
         """Convert observations into NumPy data type."""
-        # Sb3 doesn't support asymmetric observation spaces, so we only use "policy"
+        # Gymnasium doesn't support asymmetric observation spaces, so we only use "policy"
         obs = obs_dict["policy"]
-        # note: ManagerBasedRLEnv uses torch backend (by default).
+        # ManagerBasedRLEnv uses torch backend by default.
         if isinstance(obs, dict):
             for key, value in obs.items():
                 obs[key] = value.detach().cpu().numpy()
@@ -248,44 +226,37 @@ class GymIsaacWrapper(VecEnv):
         self, obs: np.ndarray, terminated: np.ndarray, truncated: np.ndarray, extras: dict, reset_ids: np.ndarray
     ) -> list[dict[str, Any]]:
         """Convert miscellaneous information into dictionary for each sub-environment."""
-        # create empty list of dictionaries to fill
+        # Create empty list of dictionaries to fill
         infos: list[dict[str, Any]] = [dict.fromkeys(extras.keys()) for _ in range(self.num_envs)]
-        # fill-in information for each sub-environment
-        # note: This loop becomes slow when number of environments is large.
+        # Fill-in information for each sub-environment
         for idx in range(self.num_envs):
-            # fill-in episode monitoring info
+            # Fill-in episode monitoring info
             if idx in reset_ids:
                 infos[idx]["episode"] = dict()
                 infos[idx]["episode"]["r"] = float(self._ep_rew_buf[idx])
                 infos[idx]["episode"]["l"] = float(self._ep_len_buf[idx])
             else:
                 infos[idx]["episode"] = None
-            # fill-in bootstrap information
+            # Fill-in bootstrap information
             infos[idx]["TimeLimit.truncated"] = truncated[idx] and not terminated[idx]
-            # fill-in information from extras
+            # Fill-in information from extras
             for key, value in extras.items():
-                # 1. remap extra episodes information safely
-                # 2. for others just store their values
                 if key == "log":
-                    # only log this data for episodes that are terminated
                     if infos[idx]["episode"] is not None:
                         for sub_key, sub_value in value.items():
                             infos[idx]["episode"][sub_key] = sub_value
                 else:
                     infos[idx][key] = value[idx]
-            # add information about terminal observation separately
+            # Add information about terminal observation separately
             if idx in reset_ids:
-                # extract terminal observations
                 if isinstance(obs, dict):
                     terminal_obs = dict.fromkeys(obs.keys())
                     for key, value in obs.items():
                         terminal_obs[key] = value[idx]
                 else:
                     terminal_obs = obs[idx]
-                # add info to dict
                 infos[idx]["terminal_observation"] = terminal_obs
             else:
                 infos[idx]["terminal_observation"] = None
-        # return list of dictionaries
         return infos
 
